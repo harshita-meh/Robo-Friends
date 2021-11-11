642d8d7a28508ca448193c94c86f154aae53be2f
Harshita Mehrotra <hharshita.mehrotra123@gmail.com>
2021-11-11 17:16:15 +0530

    adding files

---
+++ src/robots.js
@@ -0,0 +1,72 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Harshita',
+        username : 'THOR',
+        email: 'harshita123@gmail.com'
+
+    },
+    {
+        id: 2,
+        name: 'Anushk',
+        username : 'Marvel',
+        email: 'harshita123@gmail.com'
+
+    },
+    {
+        id: 3,
+        name: 'Sambhav',
+        username : 'Captain America',
+        email: 'harshita123@gmail.com'
+
+    },
+    {
+        id: 4,
+        name: 'Harshita',
+        username : 'THOR',
+        email: 'harshita123@gmail.com'
+
+    },
+    {
+        id: 5,
+        name: 'Harshita',
+        username : 'THOR',
+        email: 'harshita123@gmail.com'
+
+    },
+    {
+        id: 6,
+        name: 'Harshita',
+        username : 'THOR',
+        email: 'harshita123@gmail.com'
+
+    },
+    {
+        id: 7,
+        name: 'Harshita',
+        username : 'THOR',
+        email: 'harshita123@gmail.com'
+
+    },
+    {
+        id: 8,
+        name: 'Harshita',
+        username : 'THOR',
+        email: 'harshita123@gmail.com'
+
+    },
+    {
+        id: 9,
+        name: 'Harshita',
+        username : 'THOR',
+        email: 'harshita123@gmail.com'
+
+    },
+    {
+        id: 10,
+        name: 'Harshita',
+        username : 'THOR',
+        email: 'harshita123@gmail.com'
+
+    }
+]
